4e2ec4aeb39a6c4ae7318084d8fd1b28597a77a6
Michael Shnizer <michaels@gigaspaces.com>
2017-07-19 17:34:40 +0300

    ovs

--- scripts/ovs/ovs_connect.sh
+++ scripts/ovs/ovs_connect.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-bridge=$(ctx ${direction} instance runtime_properties bridge)
+bridge=$(ctx ${direction} node properties resource_id)
 interface_counter=$(ctx ${direction} instance runtime_properties gre_interface_counter)
 remote_ip=$(ctx ${rdirection} instance runtime_properties host_ip)
 
@@ -11,5 +11,7 @@ interface_counter=$((interface_counter+1))
 ctx ${direction} instance runtime_properties gre_interface_counter ${interface_counter}
 
 interface=gre${interface_counter}
+
+ctx logger info "Connecting  bridge:${bridge} interface:${interface} to:${remote_ip}"
 
 sudo ovs-vsctl add-port ${bridge} ${interface}  --set interface ${interface} type=gre options:${remote_ip}
--- scripts/ovs/ovs_install.sh
+++ scripts/ovs/ovs_install.sh
@@ -2,7 +2,7 @@
 
 use_external_resource=$(ctx node properties use_external_resource)
 
-if [ "$use_external_resource" = "False" ]; then
+if [ "$use_external_resource" != "True" ]; then
 
 sudo yum -y install https://repos.fedorapeople.org/repos/openstack/EOL/openstack-kilo/rdo-release-kilo-2.noarch.rpm
 
