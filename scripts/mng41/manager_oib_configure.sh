23f2de3aea38b8a27b399ec1b766addca911b341
Michael Shnizer <michaels@gigaspaces.com>
2017-09-10 14:47:41 +0300

    env update

--- scripts/mng41/manager_oib_configure.sh
+++ scripts/mng41/manager_oib_configure.sh
@@ -10,7 +10,7 @@ sudo /bin/bash -c "echo '192.168.113.0/24 via 10.10.25.253 dev br-ovs' >> /etc/s
 # generate Key
 sudo mkdir -p /etc/cloudify/.ssh/
 sudo ssh-keygen -f /etc/cloudify/.ssh/cfy-agent-kp -N ""
-sudo chown cfyuser:cfyuser /etc/cloudify/.ssh/cfy-agent-kp
+sudo chown cfyuser:cfyuser -R /etc/cloudify/.ssh
 publick_key=$(sudo cat /etc/cloudify/.ssh/cfy-agent-kp.pub)
 
 # configure route
@@ -20,16 +20,21 @@ route add -net 192.168.113.0/24 gw 10.10.25.253
 
 cfy secret create ubuntu_trusty_image -s 05bb3a46-ca32-4032-bedd-8d7ebd5c8100
 cfy secret create centos_core_image -s aee5438f-1c7c-497f-a11e-53360241cf0f
+
 cfy secret create small_image_flavor -s 4d798e17-3439-42e1-ad22-fb956ec22b54
-cfy secret create medium_image_flavor -s 62ed898b-0871-481a-9bb4-ac5f81263b33
+#cfy secret create medium_image_flavor -s 62ed898b-0871-481a-9bb4-ac5f81263b33
+cfy secret create medium_image_flavor -s 3
 cfy secret create large_image_flavor -s 62ed898b-0871-481a-9bb4-ac5f81263b33
+
 cfy secret create keystone_username -s admin
 cfy secret create keystone_password -s 'cloudify1234'
 cfy secret create keystone_tenant_name -s admin
 cfy secret create keystone_url -s http://10.10.25.1:5000/v2.0
 cfy secret create region -s RegionOne
+
 cfy secret create agent_key_private -s /etc/cloudify/.ssh/cfy-agent-kp
 cfy secret create agent_key_public --secret-file /etc/cloudify/.ssh/cfy-agent-kp.pub
+
 cfy secret create private_subnet_name -s provider_subnet
 cfy secret create private_network_name -s provider
 cfy secret create public_subnet_name -s  private_subnet
@@ -40,11 +45,16 @@ cfy secret create external_network_name -s external_network
 
 # Upload Default Plugins
 cfy plugins upload https://github.com/cloudify-incubator/cloudify-utilities-plugin/releases/download/1.2.5/cloudify_utilities_plugin-1.2.5-py27-none-linux_x86_64-centos-Core.wgn
+cfy plugins upload https://github.com/cloudify-incubator/cloudify-utilities-plugin/releases/download/1.3.0/cloudify_utilities_plugin-1.3.0-py27-none-linux_x86_64-centos-Core.wgn
+
 cfy plugins upload https://github.com/cloudify-incubator/cloudify-kubernetes-plugin/releases/download/1.0.0/cloudify_kubernetes_plugin-1.0.0-py27-none-linux_x86_64.wgn
 cfy plugins upload https://github.com/cloudify-incubator/cloudify-kubernetes-plugin/releases/download/1.1.0/cloudify_kubernetes_plugin-1.1.0-py27-none-linux_x86_64-centos-Core.wgn
 cfy plugins upload https://github.com/cloudify-incubator/cloudify-kubernetes-plugin/releases/download/1.2.0/cloudify_kubernetes_plugin-1.2.0-py27-none-linux_x86_64-centos-Core.wgn
+
 cfy plugins upload http://repository.cloudifysource.org/cloudify/wagons/cloudify-diamond-plugin/1.3.5/cloudify_diamond_plugin-1.3.5-py27-none-linux_x86_64-centos-Core.wgn
 cfy plugins upload http://repository.cloudifysource.org/cloudify/wagons/cloudify-diamond-plugin/1.3.5/cloudify_diamond_plugin-1.3.5-py27-none-linux_x86_64-Ubuntu-trusty.wgn
+
 cfy plugins upload http://repository.cloudifysource.org/cloudify/wagons/cloudify-fabric-plugin/1.5/cloudify_fabric_plugin-1.5-py27-none-linux_x86_64-centos-Core.wgn
+
 cfy plugins upload http://repository.cloudifysource.org/cloudify/wagons/cloudify-openstack-plugin/2.0.1/cloudify_openstack_plugin-2.0.1-py27-none-linux_x86_64-centos-Core.wgn
 cfy plugins upload https://github.com/cloudify-cosmo/cloudify-openstack-plugin/releases/download/2.2.0/cloudify_openstack_plugin-2.2.0-py27-none-linux_x86_64-centos-Core.wgn
