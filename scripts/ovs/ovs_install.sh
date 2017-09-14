414e3633f9716f90648aa531168ef9e03d810443
Michael Shnizer <michaels@gigaspaces.com>
2017-09-14 12:36:24 +0300

    Update ovs_install.sh

--- scripts/ovs/ovs_install.sh
+++ scripts/ovs/ovs_install.sh
@@ -4,7 +4,10 @@ use_external_resource=$(ctx node properties use_external_resource)
 
 if [ "$use_external_resource" != "True" ]; then
 
-sudo yum -y install https://repos.fedorapeople.org/repos/openstack/EOL/openstack-kilo/rdo-release-kilo-2.noarch.rpm
+#sudo yum -y install https://repos.fedorapeople.org/repos/openstack/EOL/openstack-kilo/rdo-release-kilo-2.noarch.rpm
+#
+# Use okata openstack repo to install openvswitch
+sudo yum -y install https://repos.fedorapeople.org/repos/openstack/openstack-ocata/rdo-release-ocata-3.noarch.rpm
 
 sudo yum -y install openvswitch
 
