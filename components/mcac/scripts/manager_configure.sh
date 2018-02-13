d1f95ed22cacd8f2722444a27952c18e8a141393
EarthmanT <EarthmanT@users.noreply.github.com>
2018-02-13 18:19:37 +0200

    Update manager_configure.sh

--- components/mcac/scripts/manager_configure.sh
+++ components/mcac/scripts/manager_configure.sh
@@ -10,3 +10,4 @@ env -i cfy blueprints upload -n aws.yaml              -b "mariadb-blueprint"    
 env -i cfy blueprints upload -n aws.yaml              -b "haproxy-blueprint"        "https://github.com/cloudify-examples/haproxy-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1
 env -i cfy blueprints upload -n openstack.yaml        -b "drupal-blueprint"  "https://github.com/cloudify-examples/drupal-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1
 env -i cfy blueprints upload -n blueprint.yaml        -b "db-lb-app"  "https://github.com/EarthmanT/db-lb-app/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1
+env -i cfy blueprints upload -n openstack.yaml        -b "k8s-e2e" "https://github.com/EarthmanT/e2e/releases/download/k8s-e2e-download/k8s-e2e.zip"  >> /tmp/cfy_status.txt 2>&1
