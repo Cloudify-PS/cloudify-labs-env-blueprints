a99210e566d1dd3ac3a0ff55031e9e56814a7798
EarthmanT <EarthmanT@users.noreply.github.com>
2018-02-15 11:18:33 +0200

    Change Blueprint Names
    These Blueprint Names are used in the documentation.

--- components/mcac/scripts/manager_configure.sh
+++ components/mcac/scripts/manager_configure.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-
 env -i cfy plugins upload "https://github.com/EarthmanT/cloudify-dblb/releases/download/0.2/cloudify_dblb-0.2-py27-none-linux_x86_64-centos-Core.wgn" >> /tmp/cfy_status.txt 2>&1
 
-
-
 env -i cfy blueprints upload -n update-blueprint.yaml -b "aws-example-network"        "https://github.com/cloudify-examples/aws-example-network/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1
-env -i cfy blueprints upload -n aws.yaml              -b "mariadb-blueprint"        "https://github.com/cloudify-examples/mariadb-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1
-env -i cfy blueprints upload -n aws.yaml              -b "haproxy-blueprint"        "https://github.com/cloudify-examples/haproxy-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1
-env -i cfy blueprints upload -n openstack.yaml        -b "drupal-blueprint"         "https://github.com/cloudify-examples/drupal-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1
+env -i cfy blueprints upload -n aws.yaml              -b "db"        "https://github.com/cloudify-examples/mariadb-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1
+env -i cfy blueprints upload -n aws.yaml              -b "lb"        "https://github.com/cloudify-examples/haproxy-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1
+env -i cfy blueprints upload -n openstack.yaml        -b "drupal"         "https://github.com/cloudify-examples/drupal-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1
 env -i cfy blueprints upload -n blueprint.yaml        -b "db-lb-app"                "https://github.com/EarthmanT/db-lb-app/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1
 env -i cfy blueprints upload -n openstack.yaml        -b "k8s-e2e"                  "https://github.com/EarthmanT/e2e/releases/download/k8s-e2e-download/k8s-e2e.zip"  >> /tmp/cfy_status.txt 2>&1
