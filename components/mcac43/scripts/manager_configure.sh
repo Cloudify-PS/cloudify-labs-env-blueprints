f42199ec6eb941c0f39c5c142df6e4c05eb491cd
michaelshn <michaels@cloudify.co>
2018-04-30 14:53:37 +0300

    updates

--- components/mcac43/scripts/manager_configure.sh
+++ components/mcac43/scripts/manager_configure.sh
@@ -5,8 +5,11 @@ env -i cfy plugins  bundle-upload -p https://storage.reading-a.openstack.memset.
 
 
 env -i cfy blueprints upload -n update-blueprint.yaml -b "aws-example-network"        "https://github.com/cloudify-examples/aws-example-network/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
-env -i cfy blueprints upload -n aws.yaml              -b "db"        "https://github.com/cloudify-examples/mariadb-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
-env -i cfy blueprints upload -n aws.yaml              -b "lb"        "https://github.com/cloudify-examples/haproxy-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
+env -i cfy blueprints upload -n openstack.yaml        -b "db"        "https://github.com/cloudify-examples/mariadb-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
+env -i cfy blueprints upload -n openstack.yaml        -b "lb"        "https://github.com/cloudify-examples/haproxy-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
 env -i cfy blueprints upload -n openstack.yaml        -b "drupal"         "https://github.com/cloudify-examples/drupal-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
 env -i cfy blueprints upload -n blueprint.yaml        -b "db-lb-app"                "https://github.com/EarthmanT/db-lb-app/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
 env -i cfy blueprints upload -n openstack.yaml        -b "k8s-e2e"                  "https://github.com/EarthmanT/e2e/releases/download/k8s-e2e-download/k8s-e2e.zip"  >> /tmp/cfy_status.txt 2>&1 &
+
+env -i cfy blueprints upload -n aws.yaml              -b "db-aws"        "https://github.com/cloudify-examples/mariadb-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
+env -i cfy blueprints upload -n aws.yaml              -b "lb-aws"        "https://github.com/cloudify-examples/haproxy-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
