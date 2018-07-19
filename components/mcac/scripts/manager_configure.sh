eb130ad55a2a001103319e9b6a92169d40daeb5d
earthmant <trammell@cloudify.co>
2018-07-19 15:03:51 +0300

    update to use integrated blueprint for k8s-db-lb-app

--- components/mcac/scripts/manager_configure.sh
+++ components/mcac/scripts/manager_configure.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-REPO=https://storage.reading-a.openstack.memset.com:8080/swift/v1/ca0c4540c8f84ad3917c40b432a49df8/MCAC/
-
-for plugin in `curl $REPO | grep wgn`; do
-
-ctx logger info "Uploading Plugin $plugin "
-env -i cfy plugins upload  $REPO$plugin >> /tmp/cfy_status.txt 2>&1
-
-done
+# Install latest plugins.
+env -i cfy plugins bundle-upload >> /tmp/cfy_status.txt 2>&1
 
 
-#env -i cfy plugins upload "https://github.com/EarthmanT/cloudify-dblb/releases/download/0.2/cloudify_dblb-0.2-py27-none-linux_x86_64-centos-Core.wgn" >> /tmp/cfy_status.txt 2>&1
-#env -i cfy plugins upload "https://github.com/cloudify-incubator/cloudify-awssdk-plugin/releases/download/1.2.0.3/cloudify_awssdk_plugin-1.2.0.3-py27-none-linux_x86_64-centos-Core.wgn" >> /tmp/cfy_status.txt 2>&1
-#env -i cfy plugins upload "https://github.com/cloudify-cosmo/cloudify-aws-plugin/releases/download/1.5.1.2/cloudify_aws_plugin-1.5.1.2-py27-none-linux_x86_64-centos-Core.wgn" >> /tmp/cfy_status.txt 2>&1
+# Put AWS Network Blueprint before uploading the AWS DB and LB.
+env -i cfy blueprints upload -n update-blueprint.yaml \
+    -b "aws-example-network" \
+    "https://github.com/cloudify-examples/aws-example-network/archive/4.3.2.zip" >> /tmp/cfy_status.txt 2>&1 &
 
-env -i cfy blueprints upload -n update-blueprint.yaml -b "aws-example-network"        "https://github.com/cloudify-examples/aws-example-network/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
-env -i cfy blueprints upload -n aws.yaml              -b "db"        "https://github.com/cloudify-examples/mariadb-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
-env -i cfy blueprints upload -n aws.yaml              -b "lb"        "https://github.com/cloudify-examples/haproxy-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
-env -i cfy blueprints upload -n openstack.yaml        -b "drupal"         "https://github.com/cloudify-examples/drupal-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
-env -i cfy blueprints upload -n blueprint.yaml        -b "db-lb-app"                "https://github.com/EarthmanT/db-lb-app/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
-env -i cfy blueprints upload -n openstack.yaml        -b "k8s-e2e"                  "https://github.com/EarthmanT/e2e/releases/download/k8s-e2e-download/k8s-e2e.zip"  >> /tmp/cfy_status.txt 2>&1 &
+# Install base example environment.
+env -i cfy install --timeout 1800 \
+    -i db_main_file_name=aws.yaml \
+    -i lb_main_file_name=aws.yaml \
+    -n blueprint.yaml \
+    -b "e2e" \
+    "https://github.com/EarthmanT/e2e-deployment/archive/v1.zip" >> /tmp/cfy_status.txt 2>&1 &
+
--- components/mcac43/scripts/manager_configure.sh
+++ components/mcac43/scripts/manager_configure.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+# Install current plugins bundle.
+env -i cfy plugins bundle-upload >> /tmp/cfy_status.txt 2>&1
 
-env -i cfy plugins  bundle-upload -p https://storage.reading-a.openstack.memset.com:8080/swift/v1/ca0c4540c8f84ad3917c40b432a49df8/PluginsMD/mcac-cloudify-plugins-bundel.tar.gz >> /tmp/cfy_status.txt 2>&1
+# Put AWS Network Blueprint before uploading the AWS DB and LB.
+env -i cfy blueprints upload -n update-blueprint.yaml \
+    -b "aws-example-network" \
+    "https://github.com/cloudify-examples/aws-example-network/archive/4.3.2.zip" >> /tmp/cfy_status.txt 2>&1 &
 
+# Install base example environment.
+env -i cfy install \
+    --timeout 1800 \
+    -n blueprint.yaml \
+    -b "e2e" \
+    "https://github.com/EarthmanT/e2e-deployment/archive/v1.zip" >> /tmp/cfy_status.txt 2>&1 &
 
-env -i cfy blueprints upload -n update-blueprint.yaml -b "aws-example-network"        "https://github.com/cloudify-examples/aws-example-network/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
-env -i cfy blueprints upload -n openstack.yaml        -b "db"        "https://github.com/cloudify-examples/mariadb-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
-env -i cfy blueprints upload -n openstack.yaml        -b "lb"        "https://github.com/cloudify-examples/haproxy-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
-env -i cfy blueprints upload -n openstack.yaml        -b "drupal"         "https://github.com/cloudify-examples/drupal-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
-env -i cfy blueprints upload -n blueprint.yaml        -b "db-lb-app"                "https://github.com/EarthmanT/db-lb-app/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
-env -i cfy blueprints upload -n openstack.yaml        -b "k8s-e2e"                  "https://github.com/EarthmanT/e2e/releases/download/k8s-e2e-download/k8s-e2e.zip"  >> /tmp/cfy_status.txt 2>&1 &
+# Add AWS DB Blueprint
+env -i cfy blueprints upload -n aws.yaml \
+    -b "db-aws" \
+    "https://github.com/cloudify-examples/mariadb-blueprint/archive/4.3.2.zip" >> /tmp/cfy_status.txt 2>&1 &
 
-env -i cfy blueprints upload -n aws.yaml              -b "db-aws"        "https://github.com/cloudify-examples/mariadb-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
-env -i cfy blueprints upload -n aws.yaml              -b "lb-aws"        "https://github.com/cloudify-examples/haproxy-blueprint/archive/master.zip"  >> /tmp/cfy_status.txt 2>&1 &
+# Add AWS LB Blueprint
+env -i cfy blueprints upload -n aws.yaml \
+    -b "lb-aws" \
+    "https://github.com/cloudify-examples/haproxy-blueprint/archive/4.3.2.zip" >> /tmp/cfy_status.txt 2>&1 &
