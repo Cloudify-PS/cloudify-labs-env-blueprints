3ee3ef820419974b17ce4260037098eed1ebcedc
michaelshn <michaels@cloudify.co>
2018-12-30 14:30:32 +0200

    multicloud 45

---
+++ components/mcac45/scripts/manager_configure.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#  Install current plugins bundle.
+sudo -u centos  cfy plugins bundle-upload >> /tmp/cfy_status.txt 2>&1
+
+sudo -u centos  cfy plugins upload \
+    "http://repository.cloudifysource.org/cloudify/wagons/cloudify-dblb/0.2/cloudify_dblb-0.2-py27-none-linux_x86_64-centos-Core.wgn" \
+    -y "http://www.getcloudify.org/spec/dblb/0.2/plugin.yaml"  >> /tmp/cfy_status.txt 2>&1
+
+
+# Put AWS Network Blueprint before uploading the AWS DB and LB.
+sudo -u centos  cfy blueprints upload -n update-blueprint.yaml \
+    -b "aws-example-network" \
+    "https://github.com/cloudify-examples/aws-example-network/archive/4.5.zip" >> /tmp/cfy_status.txt 2>&1
+
+# Wait for all system workflows to be compleated
+while [ "`sudo -u centos cfy executions list  --include-system-workflows | grep  "install_plugin" | grep -v compleated`" == "" ]; do sleep 1 ;echo sleeping ; done
+
+# Install base example environment.
+sudo -u centos  cfy install \
+    --timeout 1800 \
+    -n blueprint.yaml \
+    -b "e2e" \
+    "https://github.com/Cloudify-PS/e2e-deployment/archive/v2.zip" >> /tmp/cfy_status.txt 2>&1 &
+
+# Add AWS DB Blueprint
+sudo -u centos  cfy blueprints upload -n aws.yaml \
+    -b "db-aws" \
+    "https://github.com/cloudify-examples/mariadb-blueprint/archive/4.5.zip" >> /tmp/cfy_status.txt 2>&1 &
+
+# Add AWS LB Blueprint
+sudo -u centos  cfy blueprints upload -n aws.yaml \
+    -b "lb-aws" \
+    "https://github.com/cloudify-examples/haproxy-blueprint/archive/4.5.zip" >> /tmp/cfy_status.txt 2>&1 &
---
+++ components/nfv45/scripts/manager_configure.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install the webserve
+####
+sudo -u centos cfy blueprints upload -n openstack-vm-blueprint-ws.yaml -b "private-webserver-bp" https://storage.reading-a.openstack.memset.com:8080/swift/v1/ca0c4540c8f84ad3917c40b432a49df8/Blueprints/NFVLAb/private-webserver-blueprint-master.zip >> /tmp/cfy_status.txt 2>&1
+sudo -u centos cfy deployments create -b "private-webserver-bp" private-webserver >> /tmp/cfy_status.txt 2>&1
+sudo -u centos cfy executions start install -d "private-webserver"  >> /tmp/cfy_status.txt 2>&1 &
+
+# Upload blueprints
+###
+sudo -u centos cfy blueprints upload -n fortigate-vnf.yaml     -b "fortigate-vnf-bp"                    https://storage.reading-a.openstack.memset.com:8080/swift/v1/ca0c4540c8f84ad3917c40b432a49df8/Blueprints/NFVLAb/nfv-scenario-blueprint-master.zip >> /tmp/cfy_status.txt 2>&1
+sudo -u centos cfy blueprints upload -n openstack-vm-lan.yaml  -b "openstack-vnf-infra"                 https://storage.reading-a.openstack.memset.com:8080/swift/v1/ca0c4540c8f84ad3917c40b432a49df8/Blueprints/NFVLAb/nfv-scenario-blueprint-master.zip >> /tmp/cfy_status.txt 2>&1
+sudo -u centos cfy blueprints upload -n fortigate-vnf-portforward-bp.yaml -b "fortigate-portforward-bp" https://storage.reading-a.openstack.memset.com:8080/swift/v1/ca0c4540c8f84ad3917c40b432a49df8/Blueprints/NFVLAb/nfv-scenario-blueprint-master.zip >> /tmp/cfy_status.txt 2>&1
+
+# Create Deployments for Stage1
+######
+sudo -u centos cfy deployments create -b "openstack-vnf-infra" sample-openstack-vnf-infra -i 'vnf_name=sample;image_url=https://s3-eu-west-1.amazonaws.com/cloudify-labs/images/FG562-DZ.img;vnf_config_port=22' >> /tmp/cfy_status.txt 2>&1
+
+# Create deployments for Stage2&3
+#####
+sudo -u centos cfy deployments create -b "fortigate-vnf-bp" fortigate-vnf >> /tmp/cfy_status.txt 2>&1
+sudo -u centos cfy deployments create -b "fortigate-portforward-bp" fortigate-portforward >> /tmp/cfy_status.txt 2>&1
